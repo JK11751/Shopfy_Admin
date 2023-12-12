31b4c084048410cfedc35d32d457cc8e037a2a6f
gimase <kgimase@gmail.com>
2023-12-12 06:48:11 +0300

    admin side

---
+++ src/features/product/productService.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+import { config } from "../../utils/axiosconfig";
+import { base_url } from "../../utils/baseUrl";
+
+const getProducts = async () => {
+  const response = await axios.get(`${base_url}product/`);
+
+  return response.data;
+};
+const getProduct = async (id) => {
+  const response = await axios.get(`${base_url}product/${id}`, config);
+
+  return response.data;
+};
+const createProduct = async (product) => {
+  const response = await axios.post(`${base_url}product/`, product, config);
+
+  return response.data;
+};
+const deleteProduct = async (id) => {
+  const response = await axios.delete(`${base_url}product/${id}`, config);
+
+  return response.data;
+};
+const updateProduct = async (product) => {
+  const response = await axios.put(
+    `${base_url}product/${product._id}`,
+    {
+      title: product.productData.title,
+      description: product.productData.description,
+      price: product.productData.price,
+      brand: product.productData.brand,
+      category: product.productData.category,
+      tags: product.productData.tags,
+      color: product.productData.color,
+      quantity: product.productData.quantity,
+      images: product.productData.images,
+    },
+    config
+  );
+
+  return response.data;
+};
+
+const productService = {
+  getProducts,
+  createProduct,
+  deleteProduct,
+  updateProduct,
+  getProduct,
+};
+
+export default productService;
---
+++ src/features/product/productSlice.js
@@ -0,0 +1,148 @@
+import { createSlice, createAsyncThunk, createAction } from "@reduxjs/toolkit";
+import productService from "./productService";
+
+export const getProducts = createAsyncThunk(
+  "product/get-products",
+  async (thunkAPI) => {
+    try {
+      return await productService.getProducts();
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+export const createProducts = createAsyncThunk(
+  "product/create-products",
+  async (productData, thunkAPI) => {
+    try {
+      return await productService.createProduct(productData);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+export const updateAProduct = createAsyncThunk(
+  "product/update-product",
+  async (product, thunkAPI) => {
+    try {
+      return await productService.updateProduct(product);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const deleteAProduct = createAsyncThunk(
+  "product/delete-product",
+  async (id, thunkAPI) => {
+    try {
+      return await productService.deleteProduct(id);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+export const getAProduct = createAsyncThunk(
+  "product/get-product",
+  async (id, thunkAPI) => {
+    try {
+      return await productService.getProduct(id);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+export const resetState = createAction("Reset_all");
+
+const initialState = {
+  products: [],
+  isError: false,
+  isLoading: false,
+  isSuccess: false,
+  message: "",
+};
+export const productSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getProducts.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getProducts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.products = action.payload;
+      })
+      .addCase(getProducts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      })
+      .addCase(createProducts.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createProducts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.createdProduct = action.payload;
+      })
+      .addCase(createProducts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      })
+      .addCase(updateAProduct.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(updateAProduct.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.createdProduct = action.payload;
+      })
+      .addCase(updateAProduct.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      })
+      .addCase(deleteAProduct.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(deleteAProduct.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.createdProduct = action.payload;
+      })
+      .addCase(deleteAProduct.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      })
+      .addCase(getAProduct.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getAProduct.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.createdProduct = action.payload;
+      })
+      .addCase(getAProduct.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      })
+      .addCase(resetState, () => initialState);
+  },
+});
+export default productSlice.reducer;
